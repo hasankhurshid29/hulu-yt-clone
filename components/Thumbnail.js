d043b8f2cb43cdbac569048582f6f59e996346cd
Hasan Shafiq <hasan.shafique@medznmore.com>
2022-05-26 11:46:48 +0500

    Initial

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -19,7 +19,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         width={1920}
       />
       <div className='p-2'>
-        <p className='text-white truncate max-w-md'>{result.overview}</p>
+        <p className='truncate max-w-md'>{result.overview}</p>
         <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
         <p className='flex items-center opacity-0 group-hover:opacity-100'>
           {result.media_type && `${result.media_type} -`}{" "}
